27e3f43f4c1e38124f87ade391f91dda4bfe1d54
hodaifa-ech <150153831+hodaifa-ech@users.noreply.github.com>
2025-04-06 22:02:11 +0100

    update resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/hodaifa-ech",
-  resume: "https://drive.google.com/file/d/1KK4Uo33-USsp9vEF_U8VHRxb6V-RUMK6/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1lSrGezHbHKii8sz7eRNVe511Qn7R4pip/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/hodaifa-echffani-297b7b284",
   twitter: "https://twitter.com/hodaifaechffani",
   insta: "https://www.instagram.com/hodaifa_echffani/",
